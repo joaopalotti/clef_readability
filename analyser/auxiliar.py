603261db89f8354b5af4b7b6aa65ef191741f2da
Joao Palotti <joaopalotti@gmail.com>
2017-08-15 11:07:54 +0300

    Fixed placement and typo

--- analyser/auxiliar.py
+++ analyser/auxiliar.py
@@ -1,17 +1,17 @@
 import re
+from itertools import permutations
 import pandas as pd
 from sklearn import feature_selection
 
-def clip_readability(df):
+def clip_readability(df, school_cap=25):
 
     school_year_metrics = ["ari_index", "smog_index", "gunning_fog_index", "flesch_kincaid_grade_level", "coleman_liau_index"]
 
-    # Max ARI Index is 15:
     for k in df.keys():
         for metric_name in school_year_metrics:
             if metric_name in k:
-                # We clip in 20 years in school ~ College degree.
-                df[k] = df[k].clip(0.0, 20.0)
+                # We clip in 25 years in school ~ College degree.
+                df[k] = df[k].clip(0.0, school_cap)
 
     # Max flesch reading ease [0--100]:
     for k in df.keys():
@@ -29,7 +29,7 @@ def clip_readability(df):
     # http://www.readabilityformulas.com/new-dale-chall-readability-formula.php
     for k in df.keys():
         if "dale_chall_score" in k:
-            df[k] = df[k].clip(0.0, 20.0)
+            df[k] = df[k].clip(0.0, school_cap)
 
     return df
 
@@ -41,7 +41,7 @@ def mapunders(understandability):
     else:
         return 1
     """
-    if understandability < 33:
+    if understandability <= 33:
         return 0
     elif understandability <= 66:
         return 1
@@ -55,13 +55,13 @@ def normalizeFeatures(doc_features):
     keys = doc_features.keys()
 
     fields = [  # General metrics:
-                'acronyms_found', 'prefixes_found', 'sufixes_found', 'numbers_found',
-                'stopwords_found', 'eng_found', 'number_words', 'number_of_chars',
+                'acronyms_found', 'numbers_found',
+                'stopwords_found', 'eng_found', 'number_words', 'number_chars',
                 'difficult_words', 'number_polysyllable_words', 'number_syllables',
-                'longer_4', 'longer_6', 'longer_10', 'longer_13',
+                'longer_4', 'longer_6', 'longer_10', 'longer_13', 'number_sentences',
                 # Medical Vocabularies:
                 'drugbank_found', 'icd_found', 'mesh_found',
-                'chv_num', 'chv_sum', 'chv_mean'
+                'chv_num', 'chv_sum', 'chv_mean', 'sufixes_found', 'prefixes_found',
                 # html metrics:
                 'n_abbrs','n_as','n_blockquotes','n_bolds','n_cites','n_divs','n_dls','n_forms','n_h1s','n_h2s','n_h3s',
                 'n_h4s','n_h5s','n_h6s','n_hs','n_imgs', 'n_inputs', 'n_links', 'n_lists','n_ols','n_ps','n_qs', 'n_scripts',
@@ -83,8 +83,9 @@ def normalizeFeatures(doc_features):
 
                 # this is the case for the html metrics, as I did not use bs4 nor justext to preprocess the text
                 if len(suffix) == 0:
-                    # I am arbitrarily chosen the number of sentences and words from bs4, not force period
-                    suffix = "_bs4_nfp"
+                    # I am arbitrarily choosing the number of sentences and words from jst, not force period
+                    suffix = "_jst_nfp"
+                    print("Could not find suffix for %s" % (k))
 
                 doc_features[prefix + "_per_word" + suffix] = doc_features[k] / doc_features["number_words" + suffix]
                 doc_features[prefix + "_per_sentence" + suffix] = doc_features[k] / doc_features["number_sentences" + suffix]
@@ -109,7 +110,7 @@ def removeDuplicates(df):
             'sufixes_found', 'acronyms_found', 'numbers_found', 'eng_found', 'mesh_found',
             'stopwords_found', 'drugbank_found', 'icd_found', 'chv_num', 'chv_mean', 'chv_sum']
 
-    endings = {"_jst": ["_jst_fp", "_jst_nfp"], "_bs4": ["_bs4_fp","_bs4_nfp"]}
+    endings = {"_jst": ["_jst_fp", "_jst_nfp"], "_bs4": ["_bs4_fp","_bs4_nfp"], "_boi": ["_boi_fp","_boi_nfp"]}
 
     for field in duplicates:
         for ending in endings.keys():
@@ -123,6 +124,7 @@ def removeDuplicates(df):
     return df
 
 def clean(s):
+    s = re.sub('_boi', '', s)
     s = re.sub('_bs4', '', s)
     s = re.sub('_jst', '', s)
     s = re.sub('_nopreprocess', '', s)
@@ -134,11 +136,13 @@ def clean(s):
 
 def clean_preprocess(s):
     s = re.sub('_bs4', '', s)
+    s = re.sub('_boi', '', s)
     s = re.sub('_jst', '', s)
     return s
 
 def create_filters(df, field):
     df["bs4"] = df[field].apply(lambda x: "bs4" in x)
+    df["boi"] = df[field].apply(lambda x: "boi" in x)
     df["jst"] = df[field].apply(lambda x: "jst" in x)
     df["npro"] = df[field].apply(lambda x: "nopreprocess" in x)
     df["fp"] = df[field].apply(lambda x: "fp" in x)
@@ -154,4 +158,37 @@ def topFeatures(X, y, K, feature_names, score_func=feature_selection.chi2):
     fs = feature_selection.SelectKBest(score_func=score_func).fit(X,y)
     features = pd.DataFrame(zip(feature_names, fs.scores_), columns=["feature", "value"])
     return features[["feature","value"]].sort_values(by="value", ascending=False).head(K)
+
+def goodman_kruskal_gamma(m, n):
+    """
+    compute the Goodman and Kruskal gamma rank correlation coefficient;
+    this statistic ignores ties is unsuitable when the number of ties in the
+    data is high. it's also slow.
+    >> x = [2, 8, 5, 4, 2, 6, 1, 4, 5, 7, 4]
+    >> y = [3, 9, 4, 3, 1, 7, 2, 5, 6, 8, 3]
+    >> goodman_kruskal_gamma(x, y)
+    0.9166666666666666
+    from https://github.com/shilad/context-sensitive-sr/blob/master/SRSurvey/src/python/correlation.py
+    """
+    if len(m)==0  or len(n) == 0:
+        print("ERROR!!")
+        return 0.0
+
+    if min(n) == max(n) or min(m) == max(m):
+        print("No variance!!! ERROR!!")
+        return 0.0
+
+    num = 0
+    den = 0
+    for (i, j) in permutations(list(range(len(m))), 2):
+        m_dir = m[i] - m[j]
+        n_dir = n[i] - n[j]
+        sign = m_dir * n_dir
+        if sign > 0:
+            num += 1
+            den += 1
+        elif sign < 0:
+            num -= 1
+            den += 1
+    return num / float(den)
 
