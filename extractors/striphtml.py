3582b098b37b3ab7535a6adafdb6f678dca2e238
Joao Palotti <joaopalotti@gmail.com>
2017-08-27 15:49:20 +0300

    Added script to remove html boilerplate

---
+++ extractors/striphtml.py
@@ -0,0 +1,21 @@
+import glob
+from str2bool import str2bool
+import sys, os
+from auxiliar import get_content
+
+
+path_to_documents = sys.argv[1]
+out_path = sys.argv[2]
+htmlremover = sys.argv[3]
+forceperiod = str2bool(sys.argv[4])
+
+for filename in glob.glob(os.path.join(path_to_documents, "*")):
+    content = get_content(filename, htmlremover, forceperiod)
+
+    outpath = os.path.join(out_path, os.path.basename(filename))
+
+    print(filename)
+    with open(outpath, "w") as fout:
+        fout.write(content)
+
+
